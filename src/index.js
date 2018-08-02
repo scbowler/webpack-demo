54ddc6f5a8653fa6ffa8ff4aa24e2af30c972274
Scott Bowler <scott.bowler@learningfuze.com>
2018-08-02 12:59:04 -0700

    Setting up webpack in progress - breaking for lunch mmmmmm

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { login } from './helpers';
+
+login('SassySally', 'qwer');
+
+login('SomeRandom', 'ghj');
---
+++ src/users.js
@@ -0,0 +1,18 @@
+export default {
+    DiscoStu: {
+        password: 'asdf',
+        name: 'Stu Dunworth'
+    },
+    SassySally: {
+        password: 'qwer',
+        name: 'Heather Newman'
+    },
+    HopeWaits: {
+        password: 'zxcv',
+        name: 'Anna Smith',
+    },
+    DietCoke: {
+        password: 'justforthetasteofit',
+        name: 'Ben Hur'
+    }
+}
